002f777875d94dc3e6ffc735c5a18219f2135400
Pablo Duenas <pabloduenas64@gmail.com>
2022-06-21 02:15:59 +0000

    Add google map, add styling for google map, add function to display google map.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -43,3 +43,10 @@ async function displayFunFact() {
     const funFactsContainer = document.getElementById('funfacts-container');
     funFactsContainer.innerText = fact;
 }
+
+function initMap() {
+  map = new google.maps.Map(
+  document.getElementById("map"), {
+      center: { lat: 34.0522, lng: -118.2437 }, 
+      zoom: 8,});
+}
